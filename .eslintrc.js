33bacc21ba9287f4bb9bb176f089c07f92aec09c
doomsower <K.Kyznetcov@gmail.com>
2022-08-15 13:21:25 +0300

    refactor: align projects

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,4 @@
 module.exports = {
-  plugins: ["simple-import-sort", "unused-imports"],
-  extends: [
-    "alloy",
-    "alloy/typescript",
-    "plugin:import/recommended",
-    "plugin:import/typescript"
-  ],
-  env: {
-    node: true
-  },
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-floating-promises": "warn",
-    "max-params": "off",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_"
-      }
-    ]
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-      }
-    }
-  },
-  parserOptions: {
-    project: "./tsconfig.json"
-  }
+  root: true,
+  extends: ["@gearbox-protocol/eslint-config"]
 };
