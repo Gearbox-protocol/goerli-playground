6ccb9beb00a56badf75ab165e1ae08bd9424081c
doomsower <K.Kyznetcov@gmail.com>
2022-08-18 15:45:38 +0300

    fix: trading bot should be verified

--- scripts/src/TradingBotDeployer.ts
+++ scripts/src/TradingBotDeployer.ts
@@ -1,10 +1,8 @@
-import { deploy } from "@gearbox-protocol/devops";
-
 import { TradingBot } from "../../types";
 import { AbstractDeployer } from "./AbstractDeployer";
 
 export class TradingBotDeployer extends AbstractDeployer {
   protected async run(): Promise<void> {
-    await deploy<TradingBot>("TradingBot", undefined, this.syncer);
+    await this.deploy<TradingBot>("TradingBot", this.syncer);
   }
 }
